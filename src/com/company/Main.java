ff9a55d6126c439ae8df753c159fd85b15d58d81
Dosalieva <adosalieva2000@gmail.com>
2020-08-10 21:32:06 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printPhrases();
+        printPhrases();
+        printPhrases();
+        addititin ()
+
+    }
+public static void additition (int num1, int num2){
+    System.out.println(num1 = num2);
+}
+
+    public static void printPhrases() {
+        System.out.println("Geektech");
+        System.out.println("Java");
+        System.out.println("Hello");
+        test();
+    }
+
+    public static void test (){
+        int a = 9;
+        System.out.println(a+10);
+}
+}
